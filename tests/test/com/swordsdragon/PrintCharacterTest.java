b0d1b221ee8150c61d0f22a6379f8e950ad4930b
Miguel Angel <miguel.angel.lopez.vicente@gmail.com>
2016-09-13 18:42:52 +0200

    Print Character

--- tests/test/com/swordsdragon/PrintCharacterTest.java
+++ tests/test/com/swordsdragon/PrintCharacterTest.java
@@ -3,6 +3,7 @@ package test.com.swordsdragon;
 import com.swordsdragons.characters.GameCharacter;
 import com.swordsdragons.equip.Weapon;
 import com.swordsdragons.heros.Hero;
+import com.swordsdragons.potions.Potion;
 import org.junit.Test;
 
 /**
@@ -28,8 +29,31 @@ public class PrintCharacterTest {
         hero.setHp(75);
         hero.addPXs(9000);
         hero.setAttack(30);
+
+        System.out.println(hero);
+    }
+
+
+    @Test
+    public void printHeroItems() {
+        Hero hero = new Hero();
+        hero.setName("Heroe 1");
+        hero.setMaxHP(75);
+        hero.setHp(75);
+        hero.addPXs(9000);
+        hero.setAttack(30);
         hero.setWeapon(new Weapon(20));
 
+        Potion potion1 = new Potion();
+        potion1.setHp(20);
+        Potion potion2 = new Potion();
+        potion2.setHp(15);
+        Potion potion3 = new Potion();
+        potion3.setHp(50);
+
+        hero.addItem(potion1);
+        hero.addItem(potion2);
+        hero.addItem(potion3);
 
         System.out.println(hero);
     }
